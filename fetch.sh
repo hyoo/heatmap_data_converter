d956cdeffbe6a8a9fe7b2b56f88b5ac83c7784b1
Hyunseung Yoo <hsyoo@anl.gov>
2018-03-05 16:36:24 -0600

    init commit

---
+++ fetch.sh
@@ -0,0 +1,7 @@
+curl -X POST -H 'Content-Type:application/jsonrpc+json' 'https://p3.theseed.org/services/data_api/' -d @req.micro.json > micro.json
+
+# curl -X POST -H 'Content-Type:application/jsonrpc+json' 'https://p3.theseed.org/services/data_api/' -d @req.small.json > small.json
+
+# curl -X POST -H 'Content-Type:application/jsonrpc+json' 'https://p3.theseed.org/services/data_api/' -d @req.medium.json > medium.json
+
+# curl -X POST -H 'Content-Type:application/jsonrpc+json' 'https://p3.theseed.org/services/data_api/' -d @req.large.json > large.json
